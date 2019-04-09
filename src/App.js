d231288106e3032c38450c27a40f4007857041de
anjumriashat <riashat93@yahoo.com>
2019-04-09 18:34:18 +0600

    Added sorting,filtering,pagination

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
 import React, { Component } from "react";
 import "./App.css";
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 class App extends Component {
- 
-  render() {    
-    return (<p><Hello </p>);
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    const url = "https://jsonplaceholder.typicode.com/photos";
+    fetch(url, {
+      method: "GET"
+    })
+      .then(response => response.json())
+      .then(posts => {
+        this.setState({ posts: posts });
+      });
+  }
+  render() {
+    const columns = [
+      {
+        Header: "Album ID",
+        accessor: "albumId"
+      },
+      {
+        Header: "ID",
+        accessor: "id"
+      },
+      {
+        Header: "Title",
+        accessor: "title",
+        sortable: false,
+        filterable: false
+      },
+      {
+        Header: "Url",
+        accessor: "url",
+        sortable: false,
+        filterable: false
+      },
+      {
+        Header: "Thumbnail Url",
+        accessor: "thumbnailUrl",
+        sortable: false,
+        filterable: false
+      }
+    ];
+    return <ReactTable columns={columns} data={this.state.posts} filterable />;
   }
 }
 
